c87dc1db4c8ee48dabe1ac0d3868d3ff2f964e8f
hazelcharm <hazelcharm@users.noreply.github.com>
2018-12-11 18:02:14 +0100

    Update jsonParser.py

--- jsonParser.py
+++ jsonParser.py
@@ -6,13 +6,14 @@ Created on Tue Dec 4 16:28:18 2018
 """
 import json
 
-jsonFile = "D://2.json" #Name of the json input file
-outputFile = "D://2_Topics.txt" #Name of the text file-- for output
+inputFile = "H://DataSet//5.json" #Name of the json input file
+topicsFile = "H://DataSet//Outputs//5_Topics.txt" #Name of the text file-- for topics
+newJSONFile = "H://DataSet//Outputs//5_New.json" #Name of the json file-- for updatedJSON
 topicsList = []
 
 lines = []
 
-with open(jsonFile, "r", encoding="utf-8") as fin:
+with open(inputFile, "r", encoding="utf-8") as fin:
     lines = list(fin.readlines())
 
 op = "["
@@ -20,21 +21,28 @@ for l in lines:
     op += l+','
     
 op = op[:-1] + "]"
-print("Done")
+print("Reading successful")
+newJSON = "["
+
 
 decoded = list(json.loads(op))
 for item in decoded:
     if item["fullText"]:
         temp = item["topics"]
         topicsList.extend(temp)
+        newJSON += str(item) + "," +"\n"
 
+newJSON = newJSON[:-2] + "]"
 print(len(topicsList))
 
 if(len(topicsList) > 0):
-    with open(outputFile, "w", encoding="utf-8") as fout:
+    with open(topicsFile, "w", encoding="utf-8") as fout:
         for topic in topicsList:
             fout.write(topic+"\n")
     
-    print("Writing successful")    
+    with open(newJSONFile, "w", encoding="utf-8") as writer:
+        writer.write(newJSON)
+    
+    print("Writing successful - topics file & updated JSON")    
 else:
     print("No topics from this json")
