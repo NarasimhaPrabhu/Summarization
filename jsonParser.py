2987bbd6b664cff0bb65cef198b89b76168534b3
hazelcharm <hazelcharm@users.noreply.github.com>
2018-12-14 11:59:25 +0100

    Optimized code
    Parsing json items line by line (not the whole file)

--- jsonParser.py
+++ jsonParser.py
@@ -3,46 +3,37 @@
 Created on Tue Dec 4 16:28:18 2018
 
 @author: Sruthi Pasumarthy
+
+Update 1: 14 Dec 2018, Optimized code
 """
 import json
 
-inputFile = "H://DataSet//5.json" #Name of the json input file
-topicsFile = "H://DataSet//Outputs//5_Topics.txt" #Name of the text file-- for topics
-newJSONFile = "H://DataSet//Outputs//5_New.json" #Name of the json file-- for updatedJSON
+inputFile = "H://DataSet//1.json" #Name of the json input file
+topicsFile = "H://DataSet//Outputs//1_Topics.txt" #Name of the text file-- for topics
+newJSONFile = "H://DataSet//Outputs//1_New.json" #Name of the json file-- for updatedJSON
 topicsList = []
 
 lines = []
 
 with open(inputFile, "r", encoding="utf-8") as fin:
     lines = list(fin.readlines())
-
-op = "["
-for l in lines:
-    op += l+','
+    print("Reading successful")
+    for line in lines:
+        newJSON = ""
+        temp = json.loads(line)
+        if temp["fullText"]:
+            topicsList.extend(temp["topics"])
+            newJSON += str(temp) + "\n"
+        
+    if topicsList:
+        with open(topicsFile, "w", encoding="utf-8") as fout:
+            for topic in topicsList:
+                fout.write(topic)
+        
+        with open(newJSONFile, "w", encoding="utf-8") as writer:
+            writer.write(newJSON)    
+        
+        print("Writing successful - topics file & updated JSON")
     
-op = op[:-1] + "]"
-print("Reading successful")
-newJSON = "["
-
-
-decoded = list(json.loads(op))
-for item in decoded:
-    if item["fullText"]:
-        temp = item["topics"]
-        topicsList.extend(temp)
-        newJSON += str(item) + "," +"\n"
-
-newJSON = newJSON[:-2] + "]"
-print(len(topicsList))
-
-if(len(topicsList) > 0):
-    with open(topicsFile, "w", encoding="utf-8") as fout:
-        for topic in topicsList:
-            fout.write(topic+"\n")
-    
-    with open(newJSONFile, "w", encoding="utf-8") as writer:
-        writer.write(newJSON)
-    
-    print("Writing successful - topics file & updated JSON")    
-else:
-    print("No topics from this json")
+    else:
+        print("No topics from this json")
