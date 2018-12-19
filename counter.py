09ad2fea84f423198afdddae8e25322498ccfa18
hazelcharm <hazelcharm@users.noreply.github.com>
2018-12-19 19:41:20 +0100

    Removing duplicate rows from dataframe

--- counter.py
+++ counter.py
@@ -3,14 +3,13 @@
 Created on Thu Dec 13 10:42:49 2018
 
 @author: Sruthi Pasumarthy
+
+Update 1: 19 Dec 2018, Removing duplicate rows from the dataframe
 """
 import pandas as pd
 
-topicsDF = pd.read_csv("D://Topics.txt", delimiter='\n')
+topicsDF = pd.read_csv("D://fullmerge.txt", delimiter='\n')
 s = topicsDF['Topics'].value_counts().rename('Count')
 topicsDF = topicsDF.join(s, on='Topics')
-#print(topicsDF)
-#newDF = topicsDF.Topics.unique()
-#print(newDF)  
-topicsDF.to_csv("D://TopicsWithCount.csv", index=False)
-
+topicsDF = topicsDF.drop_duplicates()
+topicsDF.to_csv("D://TopicsWithCount_Sruthi.csv", index=False)
