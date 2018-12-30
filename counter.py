b07304b5690340d2bff116bf134fe6effdbd4a72
hazelcharm <hazelcharm@users.noreply.github.com>
2018-12-30 18:36:17 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -5,11 +5,15 @@ Created on Thu Dec 13 10:42:49 2018
 @author: Sruthi Pasumarthy
 
 Update 1: 19 Dec 2018, Removing duplicate rows from the dataframe
+Update 2: 30 Dec 2018, fETCHING TOP 60000 topics
 """
 import pandas as pd
 
-topicsDF = pd.read_csv("D://fullmerge.txt", delimiter='\n')
+topicsDF = pd.read_csv("D://Merged.txt", delimiter='\n')
 s = topicsDF['Topics'].value_counts().rename('Count')
 topicsDF = topicsDF.join(s, on='Topics')
 topicsDF = topicsDF.drop_duplicates()
-topicsDF.to_csv("D://TopicsWithCount_Sruthi.csv", index=False)
+topicsDF = topicsDF.sort_values(by='Count', ascending=False)
+df = topicsDF.head(60000)
+df.to_csv("D://Topics_All.csv", index=False)
+print(len(topicsDF.index))
